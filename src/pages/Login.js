4f939ef98b276438d57e55092d1ebec54699d834
luciana costa <lucianacosta.r.morais@gmail.com>
2022-03-28 19:10:54 -0300

    requisitos 2,3,4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
   return (
-    <div>Login</div>
+    <form>
+      <label htmlFor="email">
+        E-mail:
+        <input
+          id="email"
+          type="text"
+          data-testid="email-input"
+          /* name="email"
+          value={ email } */
+        />
+      </label>
+
+      <label htmlFor="senha">
+        Senha:
+        <input
+          id="senha"
+          type="text"
+          data-testid="password-input"
+          /* name="email"
+          value={ email } */
+        />
+      </label>
+
+      <button
+        id="btEnter"
+        type="button"
+        data-testid="login-submit-btn"
+        /* disabled={ isDisabled }
+        onClick={ this.btEnterClick } */
+      >
+        Enter
+      </button>
+    </form>
   );
 }
 
