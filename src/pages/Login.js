c5defe71004780684fb883e9400b2f58a3f95237
luciana costa <lucianacosta.r.morais@gmail.com>
2022-03-29 15:17:17 -0300

    requisito 5

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Login() {
+  const [inputState, setInputState] = useState({ email: '', password: '' });
+
+  const [btStatus, setBtStatus] = useState(true);
+
+  const buttonStatus = () => {
+    const { email, password } = inputState;
+    const miniSenha = 7;
+    return (
+      !email.includes('@')
+      || !email.includes('.com')
+      || password.length < miniSenha
+    );
+  };
+
+  useEffect(() => {
+    setBtStatus(buttonStatus());
+  }, [inputState]); // eslint-disable-line react-hooks/exhaustive-deps
+  // não consegui resolver o problema de lint na linha 20. help!!! tive que pegar em um site uma forma de desativar o lint da linha.
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setInputState({ ...inputState, [name]: value });
+  };
   return (
     <form>
       <label htmlFor="email">
@@ -9,8 +31,9 @@ function Login() {
           id="email"
           type="text"
           data-testid="email-input"
-          /* name="email"
-          value={ email } */
+          name="email"
+          value={ inputState.email }
+          onChange={ onInputChange }
         />
       </label>
 
@@ -20,8 +43,9 @@ function Login() {
           id="senha"
           type="text"
           data-testid="password-input"
-          /* name="email"
-          value={ email } */
+          name="password"
+          value={ inputState.password }
+          onChange={ onInputChange }
         />
       </label>
 
@@ -29,8 +53,7 @@ function Login() {
         id="btEnter"
         type="button"
         data-testid="login-submit-btn"
-        /* disabled={ isDisabled }
-        onClick={ this.btEnterClick } */
+        disabled={ btStatus }
       >
         Enter
       </button>
