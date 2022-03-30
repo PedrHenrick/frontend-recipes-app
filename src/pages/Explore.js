99e01f5ecff56d859a4df7d014aff12d8217906b
rafaelbves <60044361+rafaelbves@users.noreply.github.com>
2022-03-30 20:00:00 -0300

    bloco concluído

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
+import BottomMenu from '../components/BottomMenu';
 import HeaderPage from '../components/Header';
 
 function Explore() {
   return (
-    <HeaderPage
-      title="Explore"
-      showSearch={ false }
-    />
+    <>
+      <HeaderPage
+        title="Explore"
+        showSearch={ false }
+      />
+      <BottomMenu />
+    </>
   );
 }
 
