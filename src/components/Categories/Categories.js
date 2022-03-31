0af64828d7737277760a4f9a7a25d786d44139b6
joekliws <j.wilk@hotmail.com>
2022-03-31 00:13:20 -0300

    implementando requisitos 28 e 29

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import recipesContext from '../../context/recipesContext';
 import Button from '../Forms/Button';
-import { fetchMealsOrDrinksCategories } from '../../services/api';
+import { fetchMealsOrDrinksCategories, fetchRecipesByCategory } from '../../services/api';
 
 const MAX_CATEGORIES = 5;
+const MAX_RECIPES = 12;
 
 function Categories(props) {
   const { type } = props;
   const [categories, setCategories] = useState([]);
+  const { meal: { setMeals }, drink: { setDrinks } } = useContext(recipesContext);
 
   useEffect(() => {
     if (type === 'meals') {
@@ -27,14 +30,27 @@ function Categories(props) {
     }
   }, [type]);
 
-  console.log(categories, type);
+  const clickCategoryHandler = async (categoryName) => {
+    try {
+      const recipes = await fetchRecipesByCategory(type, categoryName);
+      if (type === 'drinks') {
+        const drinks = recipes.drinks.filter((element, index) => index < MAX_RECIPES);
+        setDrinks(drinks);
+      } else if (type === 'meals') {
+        const meals = recipes.meals.filter((element, index) => index < MAX_RECIPES);
+        setMeals(meals);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
 
   const renderCategories = () => categories.map(({ strCategory: category }, index) => (
     <Button
       key={ index }
       btnName={ category }
       dataTestid={ `${category}-category-filter` }
-      clicked={ clickCategoryHandler }
+      clicked={ () => clickCategoryHandler(category) }
     />
   ));
   return (
--- src/services/api.js
+++ src/services/api.js
@@ -78,6 +78,33 @@ export const fetchMealsOrDrinksCategories = async (type) => {
 
     const data = await fetch(url);
     const results = await data.json();
+    return results;
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+export const fetchRecipesByCategory = async (type, category) => {
+  try {
+    let url;
+    let categoryFiltered;
+    if (category.startsWith('Ordinary')) {
+      categoryFiltered = category.replace(' ', '_');
+    } else {
+      categoryFiltered = encodeURIComponent(category);
+    }
+    console.log('cat', categoryFiltered);
+
+    if (type === 'meals') {
+      url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryFiltered}`;
+    } else if (type === 'drinks') {
+      url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${categoryFiltered}`;
+    } else {
+      invalidType();
+    }
+
+    const data = await fetch(url);
+    const results = await data.json();
     console.log(results);
     return results;
   } catch (err) {
