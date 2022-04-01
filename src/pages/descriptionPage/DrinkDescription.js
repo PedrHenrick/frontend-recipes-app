84c10eb3796be30c44487e52f23cc4533323c121
Pedro Henrick <phmc@ic.ufal.br>
2022-03-31 21:55:36 -0300

    Requisito 34: Adicionando elementos vindos da api

--- src/pages/descriptionPage/DrinkDescription.js
+++ src/pages/descriptionPage/DrinkDescription.js
@@ -1,18 +1,83 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+
+// * A foto deve possuir o atributo `data-testid="recipe-photo"`; ✅
+// * O título deve possuir o atributo `data-testid="recipe-title"`; ✅
+// * O botão de compartilhar deve possuir o atributo `data-testid="share-btn"`;
+// * O botão de favoritar deve possuir o atributo `data-testid="favorite-btn"`;
+// * O texto da categoria deve possuir o atributo `data-testid="recipe-category"`; ✅
+// * Os ingredientes devem possuir o atributo `data-testid="${index}-ingredient-name-and-measure"`; ✅
+// * O texto de instruções deve possuir o atributo `data-testid="instructions"`;
+// * O vídeo, presente somente na tela de comidas, deve possuir o atributo `data-testid="video"`;
+// * O card de receitas recomendadas deve possuir o atributo `data-testid="${index}-recomendation-card"`;
+// * O botão de iniciar receita deve possuir o atributo `data-testid="start-recipe-btn"`;
 
 function DrinkDescription({ history }) {
   const id = (Number(history.location.pathname.split('/')[2]));
+  const [drinkObject, setDrinkObject] = useState([]);
+
   useEffect(() => {
     const requestAPI = async () => {
       const data = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-      const response = data.json();
-      console.log(response);
+      const response = await data.json();
+      setDrinkObject(response.drinks[0]);
     };
     requestAPI();
   }, [id]);
+
+  const arrayOfEntries = Object.entries(drinkObject);
+
+  const ingredients = arrayOfEntries.filter((ingredient) => (
+    ingredient[0].includes('strIngredient')
+  ));
+
+  const measures = arrayOfEntries.filter((measure) => (
+    measure[0].includes('strMeasure')
+  ));
+  console.log(drinkObject);
+
   return (
-    <main>lalaland</main>
+    <main>
+      { drinkObject
+        ? (
+          <section>
+            <img
+              data-testid="recipe-photo"
+              src={ drinkObject.strDrinkThumb }
+              alt={ `Imagem da bebida ${drinkObject.strDrink}` }
+              width="100px"
+            />
+            <h1
+              data-testid="recipe-title"
+            >
+              { drinkObject.strDrink }
+            </h1>
+            <h2
+              data-testid="recipe-category"
+            >
+              { drinkObject.strAlcoholic }
+            </h2>
+            <ul>
+              { ingredients.filter((ingredientsTest) => ingredientsTest[1] !== null)
+                .map((ingredient, index) => (
+                  <li
+                    key={ index }
+                    data-testid={ `${index}-ingredient-name-and-measure` }
+                  >
+                    {ingredient[1]}
+                    {': '}
+                    { measures[index][1] === null
+                      ? null
+                      : measures[index][1] }
+                  </li>
+                ))}
+            </ul>
+            <p data-testid="instructions">
+              { drinkObject.strInstructions }
+            </p>
+          </section>
+        ) : null}
+    </main>
   );
 }
 
--- src/pages/descriptionPage/FoodsDescription.js
+++ src/pages/descriptionPage/FoodsDescription.js
@@ -1,95 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function FoodsDescription() {
-  // useEffect(() => {
-  //   const requestAPI = async () => {
-  //     const data = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${}`);
-  //     const response = data.json();
-  //     console.log(response);
-  //   };
-  //   requestAPI();
-  // });
+function FoodsDescription({ history }) {
+  const id = (Number(history.location.pathname.split('/')[2]));
+  const [foodsObject, setFoodsObject] = useState([]);
+
+  useEffect(() => {
+    const requestAPI = async () => {
+      const data = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const response = await data.json();
+      setFoodsObject(response.meals[0]);
+    };
+    requestAPI();
+  }, [id]);
+
+  console.log(foodsObject);
+
+  const arrayOfEntries = Object.entries(foodsObject);
+
+  const ingredients = arrayOfEntries.filter((ingredient) => (
+    ingredient[0].includes('strIngredient')
+  ));
+
+  const measures = arrayOfEntries.filter((measure) => (
+    measure[0].includes('strMeasure')
+  ));
+  console.log(foodsObject);
+
   return (
     <main>
-      <img
-        data-testid="recipe-photo"
-        src=""
-        alt=""
-      />
-
-      <h1
-        data-testid="recipeTitle"
-      >
-        Title Item
-      </h1>
-
-      <button
-        data-testid="share-btn"
-        type="button"
-      >
-        share
-      </button>
-
-      <button
-        data-testid="favorite-btn"
-        type="button"
-      >
-        favorite
-      </button>
-
-      <h2
-        data-testid="recipe-category"
-      >
-        title category
-      </h2>
-
-      {/* <ul>
-        ingredients.map((ingredient, index) => (
-        <li
-          key={ index }
-          data-testid='${index}-ingredient-name-and-measure'
-        >
-          { ingredient }
-        </li>
-        ))
-      </ul> */}
-
-      <p
-        data-testid="instructions"
-      >
-        instructions
-      </p>
-
-      <video
-        width="300px"
-        height="200px"
-      >
-        <source src="" type="video/mp4" />
-        <track kind="captions" />
-      </video>
-
-      {/* { name === 'foods'
-        ? 'lalaland'
-        : null } */}
-
-      {/* <ul>
-      cardsOfRecomendation.map((recomendation, index) => (
-        <li
-          key={ index }
-          data-testid="${index}-recomendation-card"
-        >
-          { recomendation }
-        </li>
-      </ul> */}
-
-      <button
-        type="button"
-        data-testid="start-recipe-btn"
-      >
-        Fazer
-      </button>
+      { foodsObject
+        ? (
+          <section>
+            <img
+              data-testid="recipe-photo"
+              src={ foodsObject.strMealThumb }
+              alt={ `Imagem da comida ${foodsObject.strMeal}` }
+              width="100px"
+            />
+            <h1
+              data-testid="recipe-title"
+            >
+              { foodsObject.strMeal }
+            </h1>
+            <h2
+              data-testid="recipe-category"
+            >
+              { foodsObject.strCategory }
+            </h2>
+            <ul>
+              { ingredients.filter((ingredientsTest) => (
+                ingredientsTest[1] !== null && ingredientsTest[1] !== ''
+              )).map((ingredient, index) => (
+                <li
+                  key={ index }
+                  data-testid={ `${index}-ingredient-name-and-measure` }
+                >
+                  {ingredient[1]}
+                  {': '}
+                  { measures[index][1] === null
+                    ? null
+                    : measures[index][1] }
+                </li>
+              ))}
+            </ul>
+            <p data-testid="instructions">
+              { foodsObject.strInstructions }
+            </p>
+          </section>
+        ) : null}
     </main>
   );
 }
 
 export default FoodsDescription;
+
+FoodsDescription.defaultProps = {
+  history: {},
+};
+
+FoodsDescription.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any),
+};
