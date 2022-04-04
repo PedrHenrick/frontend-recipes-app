fd159d8e87efe18d3b645b3ddacea4daff84b261
luciana costa <lucianacosta.r.morais@gmail.com>
2022-04-04 14:35:00 -0300

    refazendo testes e adicionando o teste de login

---
+++ src/tests/mockDrinks/mockDrinksCategories.js
@@ -0,0 +1,40 @@
+export const mockDrinksCategories = {
+  // https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list API CHAMADA
+  drinks: [
+    {
+      strCategory: 'Ordinary Drink',
+    },
+    {
+      strCategory: 'Cocktail',
+    },
+    {
+      strCategory: 'Shake',
+    },
+    {
+      strCategory: 'Other/Unknown',
+    },
+    {
+      strCategory: 'Cocoa',
+    },
+    {
+      strCategory: 'Shot',
+    },
+    {
+      strCategory: 'Coffee / Tea',
+    },
+    {
+      strCategory: 'Homemade Liqueur',
+    },
+    {
+      strCategory: 'Punch / Party Drink',
+    },
+    {
+      strCategory: 'Beer',
+    },
+    {
+      strCategory: 'Soft Drink',
+    },
+  ],
+};
+
+export default mockDrinksCategories;
---
+++ src/tests/mockMeals/mockMealsCategories.js
@@ -0,0 +1,49 @@
+export const mockMealsCategories = {
+  // https://www.themealdb.com/api/json/v1/1/list.php?c=list API CHAMADA
+  meals: [
+    {
+      strCategory: 'Beef',
+    },
+    {
+      strCategory: 'Breakfast',
+    },
+    {
+      strCategory: 'Chicken',
+    },
+    {
+      strCategory: 'Dessert',
+    },
+    {
+      strCategory: 'Goat',
+    },
+    {
+      strCategory: 'Lamb',
+    },
+    {
+      strCategory: 'Miscellaneous',
+    },
+    {
+      strCategory: 'Pasta',
+    },
+    {
+      strCategory: 'Pork',
+    },
+    {
+      strCategory: 'Seafood',
+    },
+    {
+      strCategory: 'Side',
+    },
+    {
+      strCategory: 'Starter',
+    },
+    {
+      strCategory: 'Vegan',
+    },
+    {
+      strCategory: 'Vegetarian',
+    },
+  ],
+};
+
+export default mockMealsCategories;
